7e41501ab5c36df2a10db557025532c2089a0325
owonikoko Ayomide <donaykoko@yahoo.com>
2024-05-10 11:29:34 +0100

    Update SidebarRow.js

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -7,7 +7,7 @@ function SidebarRow({ selected, Icon, title }) {
   return (
     <div className={`sides ${selected && "selected"}`}>
       <Icon className="sidebar__icons" />
-      <h2 className="sidebar__title">{title}</h2>
+      <h2 className="sidebar__title">{title}</h2> 
     </div>
   );
 }                        
